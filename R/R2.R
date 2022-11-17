4d9f7d868c4715f8b6377f834fd61e3bd2ff3c37
Justin Silverman <jsilve24@gmail.com>
2022-11-17 10:11:46 -0500

    fix(r2): now follows gelman 2019 calculations

--- R/R2.R
+++ R/R2.R
@@ -14,19 +14,26 @@ r2 <- function(m, ...) {
 
 r2_internal <- function(eta.hat, eta){
   resid <- eta - eta.hat
-  ss.res <- apply(resid, c(3), function(x) sum(diag(var(x))))
-  eta.mean <- apply(eta, c(1,3), mean)
-  ss.tot <- apply(eta, c(3), function(x) sum(diag(var(x))))
+  ## variance / sum of squares being defined as total variation
+  ## (trace of covariance matrix)
+  var.res <- apply(resid, c(3), function(x) sum(diag(var(t(x)))))
+  var.tot <- apply(eta, c(3), function(x) sum(diag(var(t(x)))))
 
-  return(1-ss.res/ss.tot)
+  ## Note as we are doing this all in the latent space (under the multinomial)
+  ## it guarantees that 1-var.res/var.tot is equal to var_fit/(var_fit+var_res)
+  ## exactly as discussed in Gelman 2019
+  return(1-var.res/var.tot)
 }
 
 
 ##' @rdname r2
 ##' @details
 ##'   Calculates Posterior over Linear Model R2 as:
-##'   \deqn{1-\frac{SS_{res}}{SS_tot}}
+##'   \deqn{1-\frac{SS_{res}}{SS_{tot}}}
 ##'   where \eqn{SS} is defined in terms of trace of variances
+##' 
+##'   Method of calculating R2 is multivariate version of the Bayesian R2 proposed
+##'   by Gelman, Goodrich, Gabry, and Vehtari, 2019
 ##' @export
 r2.pibblefit <- function(m, ...){
   req(m, c("Lambda", "Eta", "X"))
@@ -40,6 +47,9 @@ r2.pibblefit <- function(m, ...){
 ##' @details
 ##'   Calculates Posterior over Basset Model R2 as:
 ##'   \deqn{1-\frac{SS_{res}}{SS_tot}}
+##' 
+##'   Method of calculating R2 is multivariate version of the Bayesian R2 proposed
+##'   by Gelman, Goodrich, Gabry, and Vehtari, 2019
 ##' @export
 r2.bassetfit <- function(m, ...){
   req(m, c("Lambda", "Eta"))
