741ca0de3586e899cc906495c711d6404697f6b1
Justin Silverman <jsilve24@gmail.com>
2023-02-17 08:50:20 -0500

    speed up r2_internal plus code reformatting of R2.R

--- R/R2.R
+++ R/R2.R
@@ -9,18 +9,23 @@ NULL
 ##' @rdname r2
 ##' @export
 r2 <- function(m, ...) {
- UseMethod("r2", m) 
+        UseMethod("r2", m)
 }
 
-r2_internal <- function(eta.hat, eta){
-  resid <- eta - eta.hat
-  ## variance / sum of squares being defined as total variation
-  ## (trace of covariance matrix)
-  var.res <- apply(resid, c(3), function(x) sum(diag(var(t(x)))))
-  var.pred <- apply(eta.hat, c(3), function(x) sum(diag(var(t(x)))))
-
-  return(var.pred/(var.pred + var.res))
+row_var <- function(x) {
+        rowSums((x - rowMeans(x))^2) / (dim(x)[2] - 1)
 }
+
+r2_internal <- function(eta.hat, eta) {
+        resid <- eta - eta.hat
+        ## variance / sum of squares being defined as total variation
+        ## (trace of covariance matrix)
+        var.res <- apply(resid, c(3), function(x) sum(row_var(x)))
+        var.pred <- apply(eta.hat, c(3), function(x) sum(row_var(x)))
+
+        return(var.pred / (var.pred + var.res))
+}
+
 
 ##' @rdname r2
 ##' @param covariates vector of indices for covariates to include in calculation of R2 (default:NULL
@@ -28,27 +33,27 @@ r2_internal <- function(eta.hat, eta){
 ##'   to zero for prediction.
 ##' @details Calculates Posterior over Linear Model R2 as: \deqn{1-\frac{SS_{res}}{SS_{tot}}} where
 ##'   \eqn{SS} is defined in terms of trace of variances
-##' 
+##'
 ##'   Method of calculating R2 is multivariate version of the Bayesian R2 proposed
 ##'   by Gelman, Goodrich, Gabry, and Vehtari, 2019
 ##' @export
-r2.pibblefit <- function(m, covariates=NULL, ...){
-  req(m, c("Lambda", "Eta", "X"))
-  newdata <- m$X
-  if (!is.null(covariates)){
-    ## Defensive
-    covariates <- unique(covariates)
-    stopifnot("covariates must be integer valued" = all(round(covariates)==covariates))
-    stopifnot("some passed covariates outside of range 1:Q" = max(covariates)<=m$Q & min(covariates)>=1)
-    ## END DEFENSE
+r2.pibblefit <- function(m, covariates = NULL, ...) {
+        req(m, c("Lambda", "Eta", "X"))
+        newdata <- m$X
+        if (!is.null(covariates)) {
+                ## Defensive
+                covariates <- unique(covariates)
+                stopifnot("covariates must be integer valued" = all(round(covariates) == covariates))
+                stopifnot("some passed covariates outside of range 1:Q" = max(covariates) <= m$Q & min(covariates) >= 1)
+                ## END DEFENSE
 
-    ## set non-included covariates to zero
+                ## set non-included covariates to zero
 
-    covariates.exclude <- setdiff(1:m$Q, covariates)
-    newdata[covariates.exclude,] <- 0
-  }
-  eta.hat <- predict(m, newdata=newdata, response="LambdaX")
-  r2_internal(eta.hat, m$Eta)
+                covariates.exclude <- setdiff(1:m$Q, covariates)
+                newdata[covariates.exclude, ] <- 0
+        }
+        eta.hat <- predict(m, newdata = newdata, response = "LambdaX")
+        r2_internal(eta.hat, m$Eta)
 }
 
 
@@ -56,13 +61,13 @@ r2.pibblefit <- function(m, covariates=NULL, ...){
 ##' @details
 ##'   Calculates Posterior over Basset Model R2 as:
 ##'   \deqn{1-\frac{SS_{res}}{SS_tot}}
-##' 
+##'
 ##'   Method of calculating R2 is multivariate version of the Bayesian R2 proposed
 ##'   by Gelman, Goodrich, Gabry, and Vehtari, 2019
 ##' @export
-r2.bassetfit <- function(m, ...){
-  req(m, c("Lambda", "Eta"))
+r2.bassetfit <- function(m, ...) {
+        req(m, c("Lambda", "Eta"))
 
-  eta.hat <- predict(m, newdata=NULL, response="Lambda")
-  r2_internal(eta.hat, m$Eta)
+        eta.hat <- predict(m, newdata = NULL, response = "Lambda")
+        r2_internal(eta.hat, m$Eta)
 }
