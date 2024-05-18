c1dc802e170ee6148bab92b5540d7e4459146600
michellepistner <pistner@psu.edu>
2024-05-17 20:59:51 -0400

    removing microbeds dependency

---
+++ R/RISK_CCFA.R
@@ -0,0 +1,13 @@
+#' Data from Gevers et al. (2014)
+#' 
+#' OTU data and metadata for 1,359 samples in a Crohn's disease study
+#' 
+#' Study is described here: \url{https://pubmed.ncbi.nlm.nih.gov/24629344/}.
+#' Data was obtained from \url{https://github.com/twbattaglia/MicrobeDS}.
+#' 
+#' @docType data
+#' @name RISK_CCFA
+#' @usage data(RISK_CCFA)
+#' @format A phyloseq object.
+#' @references Gevers D, et al. The treatment-naive microbiome in new-onset Crohn's disease. Cell Host Microbe. 2014 Mar 12;15(3):382-392. doi: 10.1016/j.chom.2014.02.005. PMID: 24629344; PMCID: PMC4059512.
+NULL
